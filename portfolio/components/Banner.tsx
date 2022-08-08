311cd3b93699d0f696fa7eb09990d7eeecc3cde1
snorresovold <snorresovold@gmail.com>
2022-08-08 02:48:31 +0200

    updates

--- portfolio/components/Banner.tsx
+++ portfolio/components/Banner.tsx
@@ -3,21 +3,23 @@ import { Link as ScrollLink } from 'react-scroll';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 import content from '../public/content/content';
+import { LangContext } from './LangContext';
 
 export default function Header() {
   const [animated, setAnimated] = useState(false);
+  const { lang, SetLang } = React.useContext(LangContext);
   useEffect(() => {
     setAnimated(true);
   }, []);
 
-  return (
+  if (lang === "no") {return (
     <div
       className="min-h-screen flex items-center justify-center dark:bg-gray-900 bg-white"
     >
       <div className="w-10/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between">
         <div className="w-full md:w-2/5">
           <LazyLoadImage
-              src={content.header.img}
+              src={content.no.header.img}
               effect="blur"
               placeholderSrc={process.env.PUBLIC_URL + '/logo512.png'}
             />
@@ -28,17 +30,50 @@ export default function Header() {
               animated ? '' : 'translate-y-10 opacity-0'
             }  transform transition duration-2000 ease-in-out text-3xl md:text-5xl font-bold`}
           >
-            {content.header.text[0]}
+            {content.no.header.text[0]}
             <br />
-            {content.header.text[1]}
+            {content.no.header.text[1]}
           </h2>
           <ScrollLink to="stack" smooth={true}>
             <button className=" animate-float bg-indigo-500 px-10 py-3 text-xl uppercase mt-10 rounded-lg">
-              {content.header.btnText}
+              {content.no.header.btnText}
             </button>
           </ScrollLink>
         </div>
       </div>
     </div>
-  );
+  );}
+  else {
+    return (
+      <div
+      className="min-h-screen flex items-center justify-center dark:bg-gray-900 bg-white"
+    >
+      <div className="w-10/12 mx-auto flex flex-col md:flex-row-reverse items-center justify-between">
+        <div className="w-full md:w-2/5">
+          <LazyLoadImage
+              src={content.en.header.img}
+              effect="blur"
+              placeholderSrc={process.env.PUBLIC_URL + '/logo512.png'}
+            />
+        </div>
+        <div className="dark:text-white text-gray-900 font-dosis text-center md:text-left">
+        <h2
+            className={`${
+              animated ? '' : 'translate-y-10 opacity-0'
+            }  transform transition duration-2000 ease-in-out text-3xl md:text-5xl font-bold`}
+          >
+            {content.en.header.text[0]}
+            <br />
+            {content.en.header.text[1]}
+          </h2>
+          <ScrollLink to="stack" smooth={true}>
+            <button className=" animate-float bg-indigo-500 px-10 py-3 text-xl uppercase mt-10 rounded-lg">
+              {content.en.header.btnText}
+            </button>
+          </ScrollLink>
+        </div>
+      </div>
+    </div>
+    )
+  }
 }
--- portfolio/public/content/content.js
+++ portfolio/public/content/content.js
@@ -4,27 +4,26 @@ export default {
             { text: "Projects", to: "Projects"},
             { text: "About me", to: "About"},
             { text: "Contact me", to: "Contact"},
-        ]
+        ],
+        header: {
+          img:
+            '/images/Snorre.png',
+          text: ['Hello!', "I am Snorre Søvold.", 'I am '],
+          btnText: 'What do i know?',
+        },
     },
     no: {
         navItems: [
             { text: "Prosjekter", to: "Projects"},
             { text: "Om meg", to: "About"},
             { text: "Kontakt meg", to: "Contact"},
-        ]
+        ],
+        header: {
+          img:
+            '/images/Snorre.png',
+          text: ['Hallo!', "Eg er Snorre Søvold.", 'Eg er '],
+          btnText: 'Hva kan jeg?',
+        },
     },
-    header: {
-      img:
-        '/images/Snorre.png',
-      text: ['Hallo!', "Eg er Snorre Søvold.", 'Eg er '],
-      typical: [
-        'IT elev. 💻',
-        2000,
-        'Youtuber. 📹',
-        2000,
-        'Koder. ',
-        2000
-      ],
-      btnText: 'Hva kan jeg?',
-    },
+
 }
